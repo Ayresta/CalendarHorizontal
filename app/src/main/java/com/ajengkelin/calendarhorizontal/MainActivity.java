4bb07cc60b50d7a957e02dd30980a3a2a2f00fb1
Ayresta <ajengayrestasakti@gmail.com>
2018-11-04 21:13:50 +0700

    First Commit

---
+++ app/src/main/java/com/ajengkelin/calendarhorizontal/MainActivity.java
@@ -0,0 +1,53 @@
+package com.ajengkelin.calendarhorizontal;
+
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.HorizontalCalendarListener;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private HorizontalCalendar horizontalCalendar;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.MONTH, 1);
+        Calendar startDate = Calendar.getInstance();
+        startDate.add(Calendar.MONTH, -1);
+
+        horizontalCalendar = new HorizontalCalendar.Builder(MainActivity.this, R.id.calendarView)
+                .startDate(startDate.getTime())
+                .endDate(endDate.getTime())
+                .datesNumberOnScreen(5)
+                .dayNameFormat("EEE")
+                .dayNumberFormat("dd")
+                .monthFormat("MMM")
+                .textSize(14f, 24f, 14f)
+                .showDayName(true)
+                .showMonthName(true)
+
+                .build();
+
+        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
+            @Override
+            public void onDateSelected(Date date, int position) {
+                Toast.makeText(MainActivity.this, DateFormat.getDateInstance().format(date) + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+}
